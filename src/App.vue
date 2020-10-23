c09526ef091d7643a2fa21ea5fb8030bf9230e5b
Proladon <proladon@gmail.com>
2020-10-23 23:05:07 +0800

    base

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,19 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <DirectoryUploader id="test" :drop="true" />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import DirectoryUploader from './components/DirectoryUploader.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    DirectoryUploader,
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+@import './assets/css/style.css';
+
 </style>
---
+++ src/components/DirectoryUploader.vue
@@ -0,0 +1,105 @@
+<template>
+    
+    <!-- Input -->
+    <label v-if="!drop"
+        :for="id"
+        class="d-uploader-label light"
+    >
+        {{ text }}
+    </label>
+    
+    <input v-if="!drop"
+        class="directory-uploader" 
+        webkitdirectory directory
+        :id="id"
+        :name="id"
+        type="file" 
+        @change="upload"
+    >
+    
+    <!-- Drop -->
+    <div v-if="drop"
+        class="drop-directory-uploader"
+        :id="id"
+        @dragenter.prevent="dragenter"
+        @dragover.prevent="dragover"
+        @drop="dropload"
+    >
+        {{ text }}
+    </div>
+
+</template>
+
+<script>
+   export default {
+       name: '',
+       props:{
+           
+           id: {
+               type: String,
+               required: true,
+           },
+
+           text:{
+               type: String,
+               default: 'Upload Directory'
+           },
+
+           style:{
+               type: String,
+               default: 'light'
+           },
+
+           drop: {
+               type: Boolean,
+               default: false
+           },
+            
+           cleanStyle: {
+               type: Boolean,
+               default: false
+           },
+       },
+
+       setup(){
+           const upload = (e) =>{
+               console.log("yes")
+               e.target.value = null
+           }
+
+           const dragenter = () => {
+
+           }
+
+           const dragover = () => {
+
+           }
+
+           const dropload = (e) => {
+               console.log(e)
+               console.log("13")
+           }
+
+           return{
+               upload,
+               dragenter,
+               dragover,
+               dropload,
+           }
+       }
+   }
+</script>
+
+<style scoped>
+
+    .d-uploader-label{
+    }
+   .directory-uploader{
+       display: none;
+   }
+
+   .drop-directory-uploader{
+       height: 50px;
+       background: palegreen;
+   }
+</style>
